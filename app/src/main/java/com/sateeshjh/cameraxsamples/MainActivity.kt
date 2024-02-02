083a53767d8672df50b5423ecb64317cae5c606c
Sateesh <sateesh.jh@gmail.com>
2024-02-02 21:36:25 +0400

    Initial permission setup

--- app/src/main/java/com/sateeshjh/cameraxsamples/MainActivity.kt
+++ app/src/main/java/com/sateeshjh/cameraxsamples/MainActivity.kt
@@ -3,36 +3,46 @@ package com.sateeshjh.cameraxsamples
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.sateeshjh.cameraxsamples.ui.theme.CameraXSamplesTheme
+import android.Manifest
 
 class MainActivity: ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (hasRequiredPermissions().not()) {
+            androidx.core.app.ActivityCompat.requestPermissions(
+                this, CAMERAX_PERMISSIONS, 0
+            )
+        }
         setContent {
             CameraXSamplesTheme {
 
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    private fun hasRequiredPermissions(): Boolean {
+        return CAMERAX_PERMISSIONS.all {
+            androidx.core.content.ContextCompat.checkSelfPermission(
+                applicationContext,
+                it
+            ) == android.content.pm.PackageManager.PERMISSION_GRANTED
+        }
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CameraXSamplesTheme {
-        Greeting("Android")
+    companion object {
+
+        private val CAMERAX_PERMISSIONS = arrayOf(
+            Manifest.permission.CAMERA,
+            Manifest.permission.RECORD_AUDIO,
+        )
     }
 }
