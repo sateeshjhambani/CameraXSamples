4e1f4ebbfc768f88642151fec0548a25eca57e4a
Sateesh <sateesh.jh@gmail.com>
2024-02-07 22:58:38 +0400

    Added the CameraPreview composable

--- app/src/main/java/com/sateeshjh/cameraxsamples/MainActivity.kt
+++ app/src/main/java/com/sateeshjh/cameraxsamples/MainActivity.kt
@@ -12,9 +12,19 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.sateeshjh.cameraxsamples.ui.theme.CameraXSamplesTheme
 import android.Manifest
+import androidx.camera.view.CameraController
+import androidx.camera.view.LifecycleCameraController
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.rememberBottomSheetScaffoldState
+import androidx.compose.runtime.remember
+import androidx.compose.ui.unit.dp
 
 class MainActivity: ComponentActivity() {
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         if (hasRequiredPermissions().not()) {
@@ -24,7 +34,32 @@ class MainActivity: ComponentActivity() {
         }
         setContent {
             CameraXSamplesTheme {
+                val scaffoldState = rememberBottomSheetScaffoldState()
+                val controller = remember {
+                    LifecycleCameraController(applicationContext).apply {
+                        setEnabledUseCases(CameraController.IMAGE_CAPTURE or CameraController.VIDEO_CAPTURE)
+                    }
+                }
 
+                BottomSheetScaffold(
+                    scaffoldState = scaffoldState,
+                    sheetPeekHeight = 0.dp,
+                    sheetContent = {
+
+                    }
+                ) { padding ->
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(padding)
+                    ) {
+                        CameraPreview(
+                            controller = controller,
+                            modifier = Modifier
+                                .fillMaxSize()
+                        )
+                    }
+                }
             }
         }
     }
