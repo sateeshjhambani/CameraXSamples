aa0de5b0839f503ab7eddf3969e5eef5bd75571f
Sateesh <sateesh.jh@gmail.com>
2024-02-08 22:39:04 +0400

    Added the record a video functionality

--- app/src/main/java/com/sateeshjh/cameraxsamples/MainActivity.kt
+++ app/src/main/java/com/sateeshjh/cameraxsamples/MainActivity.kt
@@ -1,18 +1,24 @@
 package com.sateeshjh.cameraxsamples
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.graphics.Bitmap
 import android.graphics.Matrix
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture.OnImageCapturedCallback
 import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.ImageProxy
+import androidx.camera.video.FileOutputOptions
+import androidx.camera.video.Recording
+import androidx.camera.video.VideoRecordEvent
 import androidx.camera.view.CameraController
 import androidx.camera.view.LifecycleCameraController
+import androidx.camera.view.video.AudioConfig
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
@@ -24,6 +30,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Cameraswitch
 import androidx.compose.material.icons.filled.Photo
 import androidx.compose.material.icons.filled.PhotoCamera
+import androidx.compose.material.icons.filled.Videocam
 import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -40,8 +47,11 @@ import androidx.core.content.ContextCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.sateeshjh.cameraxsamples.ui.theme.CameraXSamplesTheme
 import kotlinx.coroutines.launch
+import java.io.File
 
 class MainActivity: ComponentActivity() {
+
+    private var recording: Recording? = null
 
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -133,6 +143,19 @@ class MainActivity: ComponentActivity() {
                                     contentDescription = "Take photo"
                                 )
                             }
+                            IconButton(
+                                onClick = {
+                                    takePhoto(
+                                        controller = controller,
+                                        onPhotoTaken = { recordVideo(controller) }
+                                    )
+                                },
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.Videocam,
+                                    contentDescription = "Record video"
+                                )
+                            }
                         }
                     }
                 }
@@ -153,6 +176,8 @@ class MainActivity: ComponentActivity() {
         controller: LifecycleCameraController,
         onPhotoTaken: (Bitmap) -> Unit
     ) {
+        if (!hasRequiredPermissions()) return
+
         controller.takePicture(
             ContextCompat.getMainExecutor(applicationContext),
             object: OnImageCapturedCallback() {
@@ -183,6 +208,37 @@ class MainActivity: ComponentActivity() {
                 }
             }
         )
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun recordVideo(controller: LifecycleCameraController) {
+        if (recording != null) {
+            recording?.stop()
+            recording = null
+            return
+        }
+
+        if (!hasRequiredPermissions()) return
+
+        val outputFile = File(filesDir, "my-recording.mp4")
+        recording = controller.startRecording(
+            FileOutputOptions.Builder(outputFile).build(),
+            AudioConfig.create(true),
+            ContextCompat.getMainExecutor(applicationContext),
+        ) { event ->
+            when (event) {
+                is VideoRecordEvent.Finalize -> {
+                    if (event.hasError()) {
+                        recording?.close()
+                        recording = null
+
+                        Toast.makeText(applicationContext, "Video capture failed", Toast.LENGTH_LONG).show()
+                    } else {
+                        Toast.makeText(applicationContext, "Video capture succeeded", Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }
     }
 
     companion object {
